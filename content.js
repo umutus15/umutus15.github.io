9925249e090d3d968fb7163ffd697d1755532f25
umutus15 <82036901+umutus15@users.noreply.github.com>
2022-09-21 18:16:34 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,23 +1,34 @@
-document.getElementById("myBtn").addEventListener("click", displayDate);
+const d = new Date();
+function generateRandomDate() {
+return new Date(+(new Date()) - Math.floor(Math.random() * 10000000000));
+}
 
 document.getElementById("myBtn").addEventListener("click", displayDate);
-
-document.getElementById("myBtn2").addEventListener("click", displayDate2);
 
 document.getElementById("myBtn2").addEventListener("click", displayDate2);
 
 function displayDate() {
-  document.getElementById("changename").innerHTML = "bbcuk";
-  document.getElementById("changeid").innerHTML = "bbcnews";
-   document.getElementById("changedate").innerHTML = Date();
-  document.getElementById("changetitle").innerHTML = "Andrea Coppola said he would CONSIDER VOTING MELONI!";;
-  document.getElementById("demo").innerHTML = "RANDOM NEW STUFF";
+  $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+document.getElementById("changename").innerHTML =data.results[0].name.first+" "+data.results[0].name.last;
+ document.getElementById("changeid").innerHTML = data.results[0].login.username;
+  document.getElementById("changeimg").src = data.results[0].picture.thumbnail
+  }
+});
+   document.getElementById("changedate").innerHTML = (new generateRandomDate()).toLocaleDateString('en-US');
 }
 
 function displayDate2() {
-  document.getElementById("changename").innerHTML = "cnnuk";
-  document.getElementById("changeid").innerHTML = "cnnnews";
-   document.getElementById("changedate").innerHTML = Date();
-  document.getElementById("changetitle").innerHTML = "Umut Fidan HAS BEEN CAUGHT playbacking a C13#9 chord!";;
-  document.getElementById("demo").innerHTML = "RANDOM NEW STUFF";
+  $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+document.getElementById("changename").innerHTML =data.results[0].name.first+" "+data.results[0].name.last;
+ document.getElementById("changeid").innerHTML = data.results[0].login.username;
+ document.getElementById("changeimg").src = data.results[0].picture.thumbnail
+  }
+});
+   document.getElementById("changedate").innerHTML = (new generateRandomDate()).toLocaleDateString('en-US');
 }
