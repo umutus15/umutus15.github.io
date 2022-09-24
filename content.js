beb9b6a4fa566bde829d23f8af88c54c63c0d3a6
umutus15 <umut.fidan@mail.polimi.it>
2022-09-24 16:03:30 +0300

    comments/new funcs

--- content.js
+++ content.js
@@ -1,70 +1,69 @@
 console.log("JS beginning")
 
-var json_data = {};
-var random_entry = 0;
-var tot_length;
+var json_data = {}; //where our news titles are stored
+var random_entry = 0; //var for picking random news
+var tot_length; // total entries in database
 
-$.ajax({
+$.ajax({ //download the news database, pring how many entries there are
         url: "fake-news-filtered.json",
         async: false,
         dataType: 'json',
         success: function(json) {
-            json_data = json;
-            console.log("Sucessfully downloaded data..."); // this will show the info it in firebug console
+            json_data = json; //pass it into global var
+            console.log("Sucessfully downloaded data...");
             tot_length = JSON.parse(JSON.stringify(json)).length;
             console.log("Total entries: "+tot_length);
             random_entry = getRandomInt(tot_length);
-            if(json[random_entry].reliability === "fake"){
-              console.log("Its fake");
-            }
-            else{
-              console.log("Its true");
-            }
         }
 });
+//console.log(json_data[random_entry].title)//testing
 
-//console.log(json_data[random_entry].title)
 
-const d = new Date();
-function generateRandomDate() {
-return new Date(+(new Date()) - Math.floor(Math.random() * 10000000000));
+//Associate buttons with functions
+document.getElementById("myBtn").addEventListener("click", real_button);
+
+document.getElementById("myBtn2").addEventListener("click", fake_button);
+
+const d = new Date(); // date variable for tweet date
+
+function real_button() {
+  console.log("real button")
+  change_news();
 }
 
-document.getElementById("myBtn").addEventListener("click", displayDate);
+function fake_button() {
+  console.log("fake button");
+  change_news();
+}
 
-document.getElementById("myBtn2").addEventListener("click", displayDate2);
+// Helper functions:
+function change_news(){
+  $.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+        document.getElementById("changename").innerHTML =data.results[0].name.first+" "+data.results[0].name.last;//change name of person
+        document.getElementById("changeid").innerHTML = data.results[0].login.username;//change username
+        document.getElementById("changeimg").src = data.results[0].picture.thumbnail;//change profile picture
+        random_entry = getRandomInt(tot_length);//get random number for database
+        document.getElementById("changetitle").innerHTML = json_data[random_entry].title; //change title, get random entry from database
+    }
+  });
+  document.getElementById("changedate").innerHTML = (new generateRandomDate()).toLocaleDateString('en-US'); //change date of the tweet
+}
 
-function getRandomInt(max) {
+function generateRandomDate() { // Func for generating date between two days
+  return new Date(+(new Date()) - Math.floor(Math.random() * 10000000000));
+}
+function getRandomInt(max) { // Func for getting random int until max value provided
   return Math.floor(Math.random() * max);
 }
 
-function displayDate() {
-  $.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-  document.getElementById("changename").innerHTML =data.results[0].name.first+" "+data.results[0].name.last;
-  document.getElementById("changeid").innerHTML = data.results[0].login.username;
-  document.getElementById("changeimg").src = data.results[0].picture.thumbnail
-  random_entry = getRandomInt(tot_length);
-  document.getElementById("changetitle").innerHTML = json_data[random_entry].title;
-  }
-});
-   document.getElementById("changedate").innerHTML = (new generateRandomDate()).toLocaleDateString('en-US');
-}
-
-function displayDate2() {
-  console.log("???");
-  $.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
- document.getElementById("changename").innerHTML =data.results[0].name.first+" "+data.results[0].name.last;
- document.getElementById("changeid").innerHTML = data.results[0].login.username;
- document.getElementById("changeimg").src = data.results[0].picture.thumbnail;
- random_entry = getRandomInt(tot_length);
- document.getElementById("changetitle").innerHTML = json_data[random_entry].title;
-  }
-});
-   document.getElementById("changedate").innerHTML = (new generateRandomDate()).toLocaleDateString('en-US');
+function fake_or_real(random_entry){ //func for detecting fake or real
+            if(json[random_entry].reliability === "fake"){ 
+              return 0
+            }
+            else{
+              return 1
+            }
 }
